1d9a9bc65be45359e012d54e3930199a98348022
Osama <try2rym@yahoo.com>
2021-06-14 15:03:40 +0100

    Added google fonts.

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-const { urlencoded } = require('express')
-const express = require('express')
+const express = require("express");
 
+const app = express();
 
+app.use(express.static(__dirname + "/public"));
 
-
-const app = express()
-
-
-app.use(express.static(__dirname + '/public'))
-
-
-const port = 3000
+const port = 8080;
 
 //Home
-app.get('/', (req, res)=> {
-    res.sendFile(__dirname + '/views/home.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
 
 //Projects
-app.get('/Projects', (req, res)=> {
-    // res.send('Project Page')
-})
+app.get("/Projects", (req, res) => {
+  // res.send('Project Page')
+});
 
-
-app.listen(port, ()=> {
-    console.log(`App running at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`App running at port ${port}`);
+});
