45a61136e7ceba5756eb4132588a3d09e869aeeb
Osama <try2rym@yahoo.com>
2021-06-19 16:49:31 +0100

    added Animation to Menu Option

--- public/js/f.js
+++ public/js/f.js
@@ -1,16 +1,19 @@
 const menu = document.querySelector(".menuBtn");
 const cover = document.querySelector(".cover");
+const options = document.querySelector(".options");
 
 cover.style.display = "none";
 
 menu.addEventListener("click", () => {
   if (cover.style.display === "none") {
     cover.style.display = "flex";
+    options.classList.add("animate");
     menu.style.background = "url(/image/closeBtn.png) no-repeat";
     menu.style.backgroundSize = "contain";
   } else {
     cover.style.display = "none";
     menu.style.background = "url(/image/MenuBtn.png)  no-repeat";
     menu.style.backgroundSize = "contain";
+    options.classList.remove("animate");
   }
 });
