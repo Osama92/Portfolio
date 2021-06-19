90c83d4bfdd99f196c44d5ad309380790ac87290
Osama <try2rym@yahoo.com>
2021-06-19 15:02:40 +0100

    Added HD_Btns

--- public/js/f.js
+++ public/js/f.js
@@ -1,10 +1,14 @@
-const menu = document.querySelector(".open");
+const menu = document.querySelector(".menuBtn");
+const cover = document.querySelector(".cover");
 
-menu.addEventListener("click", function () {
-  const cover = document.querySelector(".cover");
+menu.addEventListener("click", () => {
   if (cover.style.display === "none") {
     cover.style.display = "flex";
+    menu.style.background = "url(/image/closeBtn.png) no-repeat";
+    menu.style.backgroundSize = "contain";
   } else {
     cover.style.display = "none";
+    menu.style.background = "url(/image/MenuBtn.png)  no-repeat";
+    menu.style.backgroundSize = "contain";
   }
 });
